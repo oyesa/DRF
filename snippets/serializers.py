01898c8aa7cdcfc7f5aa8b4a2d8dba3fe5ebec1b
oyesa <mercyoyesa2018@gmail.com>
2022-06-09 11:58:25 +0300

    add login to browserable API

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -4,11 +4,12 @@ from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username') 
 
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos',
-                  'language', 'style', )
+                  'language', 'style', 'owner', )
 
 
 class UserSerializer(serializers.ModelSerializer):
--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Snippet
 from .serializers import SnippetSerializer
 from django.contrib.auth.models import User
@@ -12,11 +12,16 @@ from .serializers import SnippetSerializer, UserSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,) 
+
+    def perform_create(self, serializer): 
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,) 
 
 
 class UserList(generics.ListAPIView): 
